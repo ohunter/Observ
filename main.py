dc20b2e39966420337740d966023243a46e9eba6
ohunter <ohoffs@gmail.com>
2020-08-19 08:22:45 -0600

    restructured scheduler class

--- main.py
+++ main.py
@@ -22,36 +22,9 @@ tile_dict = {
     "text": ob.text_tile.from_conf,
 }
 
-def _factorize(val):
-    factors = {}
-    while val > 1:
-        for i in range(2, val+1):
-            if val % i == 0:
-                factors[i] = factors[i] + 1 if i in factors else 1
-                val //= i
-                break
-    return factors
-
-def _lcm(vals: Iterable[int]) -> int:
-    lcm = 1
-    factors = [_factorize(round(x)) for x in vals]
-    lcm_factors = {k: max([d.get(k, 0) for d in factors]) for k in dict.fromkeys([y for x in factors for y in x.keys()]).keys()}
-    [lcm := lcm * k ** v for k,v in lcm_factors.items()]
-    return lcm
-
-def _gcd(vals: Iterable[int]) -> int:
-    gcd = 1
-    factors = [_factorize(round(x)) for x in vals]
-    gcd_factors = {k: min([d.get(k, 0) for d in factors]) for k in dict.fromkeys([y for x in factors for y in x.keys()]).keys()}
-    [gcd := gcd * k ** v for k,v in gcd_factors.items()]
-    return gcd
-
-def find_period(vals: Iterable[int]) -> float:
-    return 1/_gcd(vals)
-
 class scheduler():
     def __init__(self, timing: Iterable[Tuple[int, ob.tile]]) -> None:
-        period = find_period([x for x,v in timing])
+        period = self._find_period([x for x,v in timing])
         steps = [{i/k : v for i in range(1, int(period * k)+1)} for k, v in timing]
         self.timings = {x:chain([d[x] for d in steps if x in d]) for x in dict.fromkeys(sorted([b for a in steps for b in a]))}
 
@@ -70,6 +43,33 @@ class scheduler():
 
             self.total += self.dt
             yield self.dt, self.timings.get(self.t, self.timings[max(self.timings)])
+
+    def _factorize(self, val):
+        factors = {}
+        while val > 1:
+            for i in range(2, val+1):
+                if val % i == 0:
+                    factors[i] = factors[i] + 1 if i in factors else 1
+                    val //= i
+                    break
+        return factors
+
+    def _lcm(self, vals: Iterable[int]) -> int:
+        lcm = 1
+        factors = [self._factorize(round(x)) for x in vals]
+        lcm_factors = {k: max([d.get(k, 0) for d in factors]) for k in dict.fromkeys([y for x in factors for y in x.keys()]).keys()}
+        [lcm := lcm * k ** v for k,v in lcm_factors.items()]
+        return lcm
+
+    def _gcd(self, vals: Iterable[int]) -> int:
+        gcd = 1
+        factors = [self._factorize(round(x)) for x in vals]
+        gcd_factors = {k: min([d.get(k, 0) for d in factors]) for k in dict.fromkeys([y for x in factors for y in x.keys()]).keys()}
+        [gcd := gcd * k ** v for k,v in gcd_factors.items()]
+        return gcd
+
+    def _find_period(self, vals: Iterable[int]) -> float:
+        return 1/self._gcd(vals)
 
     def __iter__(self):
         return self.next_timing()

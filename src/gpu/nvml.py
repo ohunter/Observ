dd03c66aad1098f57e8451cc1ab039302d3ee0b8
ohunter <ohoffs@gmail.com>
2020-09-08 09:08:19 -0600

    Further implementation of wrapper around NVML

--- src/gpu/nvml.py
+++ src/gpu/nvml.py
@@ -1,13 +1,11 @@
 import ctypes as ct
-from ctypes import POINTER
 import glob
 import os
 import pathlib
-from enum import Enum
-from typing import Any, Callable, Dict, Iterable, Type, Union
+from enum import Enum, IntEnum, auto, unique
+from typing import Callable, Dict, Tuple, Union
 
-
-_nvml_result_t = ct.c_int
+### Enums
 class nvml_result_t(Enum):
     NVML_SUCCESS = 0
     NVML_ERROR_UNINITIALIZED = 1
@@ -35,29 +33,126 @@ class nvml_result_t(Enum):
     NVML_ERROR_INSUFFICIENT_RESOURCES = 23
     NVML_ERROR_UNKNOWN = 999
 
-class nvml_device_t(ct.Structure):
-    pass
+class nvml_temperature_sensors_t(Enum):
+    NVML_TEMPERATURE_GPU = 0
+    NVML_TEMPERATURE_COUNT = auto()
 
-class Device():
-    def __init__(self, funcs: Iterable[Callable[..., Any]], args: Dict[str, Iterable[Any]]) -> None:
-        self._funcs = funcs
-        self._args = args
+class nvml_clock_type_t(Enum):
+    NVML_CLOCK_GRAPHICS = 0
+    NVML_CLOCK_SM = 1
+    NVML_CLOCK_MEM = 2
+    NVML_CLOCK_VIDEO = 3
+    NVML_CLOCK_COUNT = auto()
+
+### Structures
+class nvml_device_t_struct(ct.Structure):
+    pass
+nvml_device_t = ct.POINTER(nvml_device_t_struct)
+
+class nvml_memory_t(ct.Structure):
+    _fields_ = [
+        ("free", ct.c_ulonglong),
+        ("total", ct.c_ulonglong),
+        ("used", ct.c_ulonglong)
+    ]
+
+class nvml_utilization_t(ct.Structure):
+    _fields_ = [
+        ("gpu", ct.c_uint),
+        ("memory", ct.c_uint)
+    ]
+@unique
+class NVML_STATUS(IntEnum):
+    Unknown = 0
+    Loaded = 1
+    Initialized = 2
+    Shutdown = 3
+
+    def __gt__(self, x) -> bool:
+        if isinstance(x, NVML_STATUS):
+            return self.value > x.value
+        else:
+            return self.value > x
+
+    def __lt__(self, x) -> bool:
+        if isinstance(x, NVML_STATUS):
+            return self.value < x.value
+        else:
+            return self.value < x
+
+    def __eq__(self, x) -> bool:
+        if isinstance(x, NVML_STATUS):
+            return self.value == x.value
+        else:
+            return self.value == x
+
+    def __ne__(self, x) -> bool:
+        if isinstance(x, NVML_STATUS):
+            return not self.value == x.value
+        else:
+            return not self.value == x
+
+class Nvidia_GPU():
+    def __init__(self, library, device_index: int, device_ptr: nvml_device_t) -> None:
+        self._lib = library
+        self._index = device_index
+        self._ptr = device_ptr
 
     @property
-    def name(self):
-        func = [x for x in self._funcs if x.__name__ == "nvmlDeviceGetName"][0]
-        result = nvml_result_t(func(*self._args["nvmlDeviceGetName"]))
-        assert result == nvml_result_t.NVML_SUCCESS, f"Function {func.__name__} returned with {result.name}"
-        return str(self._args["nvmlDeviceGetName"][1].value, "ascii")
+    def name(self) -> str:
+        """The GPU's general name"""
+        name = ct.create_string_buffer(64)
+        self._lib.execute("nvmlDeviceGetName", self._ptr, name, ct.sizeof(name))
+        return str(name.value, "ascii")
+    @property
+    def memory(self) -> Tuple[int, int, int]:
+        """The GPU's current amount of free, total, and used memory respectively"""
+        mem = nvml_memory_t()
+        self._lib.execute("nvmlDeviceGetMemoryInfo", self._ptr, ct.byref(mem))
+        return (mem.free, mem.total, mem.used)
+    @property
+    def temperature(self) -> int:
+        """The GPU's current temperature in celsius"""
+        temp = ct.c_uint(0)
+        self._lib.execute("nvmlDeviceGetTemperature", self._ptr, nvml_temperature_sensors_t(0).value, ct.byref(temp))
+        return temp.value
+    @property
+    def power(self) -> int:
+        """The GPU's current powerdraw in milliwatts"""
+        power = ct.c_uint(0)
+        self._lib.execute("nvmlDeviceGetPowerUsage", self._ptr, ct.byref(power))
+        return power.value
+    @property
+    def fan_speed(self) -> int:
+        """The GPU's current fan speed as a percentage of the max fan speed"""
+        fan_speed = ct.c_uint(0)
+        self._lib.execute("nvmlDeviceGetFanSpeed", self._ptr, ct.byref(fan_speed))
+        return fan_speed.value
+    @property
+    def clock_speed(self) -> Tuple[int, int]:
+        """The GPU's current and max clock speed respectively"""
+        cur_clock = ct.c_uint(0)
+        max_clock = ct.c_uint(0)
+        self._lib.execute("nvmlDeviceGetClockInfo", self._ptr, nvml_clock_type_t.NVML_CLOCK_GRAPHICS.value, ct.byref(cur_clock))
+        self._lib.execute("nvmlDeviceGetMaxClockInfo", self._ptr, nvml_clock_type_t.NVML_CLOCK_GRAPHICS.value, ct.byref(max_clock))
+
+        return cur_clock.value, max_clock.value
+    @property
+    def utilization(self) -> Tuple[int, int]:
+        util = nvml_utilization_t()
+        self._lib.execute("nvmlDeviceGetUtilizationRates", self._ptr, ct.byref(util))
+
+        return util.gpu, util.memory
+
 class NVML():
     def __init__(self) -> None:
-        self._loaded = False
-        self._initialized = False
+        self._status = NVML_STATUS(0)
         self._result: _nvml_result_t
         self._created_devices = []
+        self._funcs: Dict[str, Callable[..., nvml_result_t]] = {}
 
     def setup(self, path: Union[str, os.PathLike, None] = None):
-        assert not self.loaded, "NVML can only be loaded once"
+        assert self.status == NVML_STATUS(0), "NVML can only be loaded once"
 
         self._path: os.PathLike
         if isinstance(path, str):
@@ -75,71 +170,79 @@ class NVML():
             self._path = path
 
         self._lib = ct.CDLL(self._path)
-        self._loaded = True
+        self._status += 1
 
     def initialize(self):
-        assert self.loaded, "NVML has to be loaded before initialized"
-        assert not self.initialized, "NVML can only be initialized once"
+        assert self.status > NVML_STATUS(0), "NVML has to be loaded before initialized"
+        assert self.status != NVML_STATUS(2), "NVML has already been initialized"
+        assert self.status < NVML_STATUS(3), "NVML can not be initialized after shutdown"
 
-        funcs = [
-            self._lib.nvmlInit,
-            self._lib.nvmlDeviceGetCount,
-            self._lib.nvmlDeviceGetHandleByIndex,
-            self._lib.nvmlDeviceGetName,
-            self._lib.nvmlShutdown,
-        ]
+        for func in [
+            "nvmlInit_v2",
+            "nvmlDeviceGetCount_v2",
+            "nvmlDeviceGetHandleByIndex_v2",
+            "nvmlDeviceGetName",
+            "nvmlDeviceGetMemoryInfo",
+            "nvmlDeviceGetTemperature",
+            "nvmlDeviceGetPowerUsage",
+            "nvmlDeviceGetFanSpeed",
+            "nvmlDeviceGetClockInfo",
+            "nvmlDeviceGetMaxClockInfo",
+            "nvmlDeviceGetUtilizationRates",
+            "nvmlShutdown",
+        ]:
+            self.load_function(func)
 
-        for func in funcs:
-            func.restype = _nvml_result_t
+        self.execute("nvmlInit_v2")
 
-        self._result = nvml_result_t(self._lib.nvmlInit())
-
-        assert self.result == nvml_result_t.NVML_SUCCESS, f"Function nvmlInit() returned {self.result.name}"
-
-        self._initialized = True
+        self._status += 1
 
     def shutdown(self):
-        assert self.loaded, "NVML has to be loaded before being shutdown"
-        assert self.initialized, "NVML has to be initialized before being shutdown"
+        assert self.status > NVML_STATUS(0), "NVML has to be loaded before it can be shutdown"
+        assert self.status > NVML_STATUS(2), "NVML has to be initialized before it can be shutdown"
+        assert self.status != NVML_STATUS(3), "NVML has already been shutdown"
 
-        self._result = nvml_result_t(self._lib.nvmlShutdown())
+        self.execute("nvmlShutdown")
 
-        assert self.result == nvml_result_t.NVML_SUCCESS, f"Function nvmlShutdown() returned {self.result.name}"
+        self._status += 1
 
-        self.initialized = False
+    def load_function(self, function_name: str):
+        assert self.status >= NVML_STATUS(1), "NVML has to be loaded before functions can be loaded"
+        func = getattr(self._lib, function_name)
+        func.restype = nvml_result_t
+        self._funcs[func.__name__] = func
 
-    def device(self, i: int = 0):
-        assert self.loaded and self.initialized, "NVML has to be loaded and initialized before device count is known"
+    def execute(self, function_name: str, *args):
+        if not function_name.startswith("nvmlInit"):
+            assert self.status == NVML_STATUS(2), "NVML has to be initialized before functions can be executed"
+            assert self.status < NVML_STATUS(3), "NVML can not be shutdown when executing functions"
+        assert function_name in self._funcs, "Attempted execution of unknown function"
+
+        self._result = nvml_result_t(self._funcs[function_name](*args))
+
+        assert self.result == nvml_result_t.NVML_SUCCESS, f"Function {function_name}() returned {self.result.name}"
+
+    def device(self, i: int = 0) -> Nvidia_GPU:
+        assert self.status == NVML_STATUS(2), "NVML has to be initialized before devices can be retrieved"
         assert i < self.device_count, "Requested device index is greater than the number of devices"
 
         device = nvml_device_t()
-        self._result = nvml_result_t(self._lib.nvmlDeviceGetHandleByIndex(ct.c_uint(i), ct.byref(device)))
+        self.execute("nvmlDeviceGetHandleByIndex_v2", ct.c_uint(i), ct.byref(device))
+        self._created_devices.append(Nvidia_GPU(self, i, device))
 
-        assert self.result == nvml_result_t.NVML_SUCCESS, f"Function nvmlDeviceGetHandleByIndex() returned {self.result.name}"
-
-        name = ct.create_string_buffer(64)
-
-        funcs = [self._lib.nvmlDeviceGetName]
-        args = {self._lib.nvmlDeviceGetName.__name__: [ct.byref(device), name, ct.sizeof(name)]}
-        self._created_devices.append(Device(funcs, args))
         return self._created_devices[-1]
 
     @property
-    def loaded(self) -> bool:
-        return self._loaded
-    @property
-    def initialized(self) -> bool:
-        return self._initialized
+    def status(self):
+        return self._status
     @property
     def device_count(self) -> int:
-        assert self.loaded and self.initialized, "NVML has to be loaded and initialized before device count is known"
+        assert self.status == NVML_STATUS(2), "NVML has to be initialized before device count is known"
         dev_count = ct.c_uint(0)
-        self._result = nvml_result_t(self._lib.nvmlDeviceGetCount(ct.byref(dev_count)))
-
-        assert self.result == nvml_result_t.NVML_SUCCESS, f"Function nvmlDeviceGetCount() returned {self.result.name}"
+        self.execute("nvmlDeviceGetCount_v2", ct.byref(dev_count))
         return dev_count.value
     @property
-    def result(self) -> _nvml_result_t:
+    def result(self) -> nvml_result_t:
         return self._result
 
 if __name__ == "__main__":
@@ -148,3 +251,9 @@ if __name__ == "__main__":
     n.initialize()
     dev = n.device(0)
     print(dev.name)
+    print(dev.memory)
+    print(dev.temperature)
+    print(dev.power)
+    print(dev.fan_speed)
+    print(dev.clock_speed)
+    print(dev.utilization)
